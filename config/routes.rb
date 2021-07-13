dabaabf505de558120d4a728b30175fa29289cc0
bogdanstefann <bogdanstefann@gmail.com>
2021-07-13 20:25:45 +0300

    Added navigation, resources for user and messages, added index for chatroom and messages partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   root 'chatroom#index'
 
+  resources :users, except: [:new]
+  resources :messages
+
   get 'login', to: 'sessions#new'
   # post 'login', to: 'sessions#create'
   # delete 'logout', to: 'sessions#destroy'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "nelboy", password: "password")
+User.create(username: "nelboy1", password: "password")
+User.create(username: "nelboy2", password: "password")
+User.create(username: "nelboy3", password: "password")
+User.create(username: "nelboy4", password: "password")
+
+
+Message.create(body: "A simple message1", user_id: User.first.id)
+Message.create(body: "A simple message2", user_id: User.second.id)
+Message.create(body: "A simple message3", user_id: User.first.id)
+Message.create(body: "A simple message4", user_id: User.last.id)
+Message.create(body: "A simple message5", user_id: User.last.id)
