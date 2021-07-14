10035d800c72ab76ca62aac844b13d34b4544bc9
bogdanstefann <bogdanstefann@gmail.com>
2021-07-14 13:04:42 +0300

    Added ActionCable and layout for chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   
   root 'chatroom#index'
 
-  resources :users, except: [:new]
-  resources :messages
-
   get 'login', to: 'sessions#new'
-  # post 'login', to: 'sessions#create'
-  # delete 'logout', to: 'sessions#destroy'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 
 end
